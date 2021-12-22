e1beccec173cd3578746bae9ce7fcf00ef276624
Igor Oliveira de Sousa <igor_sousa@email.com>
2021-12-21 23:06:39 -0300

    Tests AR

--- webxr/app.js
+++ webxr/app.js
@@ -1,6 +1,6 @@
 import * as THREE from '../libs/three125/three.module.js';
-import { GLTFLoader } from '../libs/three/jsm/GLTFLoader.js';
-import { RGBELoader } from '../libs/three/jsm/RGBELoader.js';
+import { GLTFLoader } from '../libs/three125/jsm/GLTFLoader.js';
+import { RGBELoader } from '../libs/three125/jsm/RGBELoader.js';
 import { ARButton } from '../libs/ARButton.js';
 import { LoadingBar } from '../libs/LoadingBar.js';
 
